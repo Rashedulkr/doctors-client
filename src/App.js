4d6e8885c7ffa3b54eeee969b8db992b5a2c1b2a
Your full name <your_github_email_@email.com>
2022-07-17 05:32:25 +0600

    karim

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 function App() {
   return (
     <div >
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+
       </Routes>
 
 
